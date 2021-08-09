4199a98081430a9480d9d502c24f296a201c3949
Doersoft <doerssoft@gmail.com>
2021-08-09 13:48:30 +0545

    forget password done

--- learningsite/learningsite/settings.py
+++ learningsite/learningsite/settings.py
@@ -142,3 +142,11 @@ CKEDITOR_CONFIGS = {
 
     },
 }
+
+# Mail Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'place your mail here' #official mail from which the reset link wll besent to the users requesting for the reset.
+EMAIL_HOST_PASSWORD = 'place your mail password here' #actual password of your mail address
--- learningsite/user/urls.py
+++ learningsite/user/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -7,4 +8,9 @@ urlpatterns = [
     path('login/', views.loginpage, name='login'),
     path('logout/', views.logoutpage, name='logout'),
     path('register/', views.registerpage, name='register'),
+
+    path('pass_reset/', auth_views.PasswordResetView.as_view(template_name="user/password_reset.html"), name="reset_password"),
+    path('pass_reset_sent/', auth_views.PasswordResetDoneView.as_view(template_name="user/password_reset_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="user/password_reset_form.html"), name="password_reset_confirm"),
+    path('pass_reset_done/', auth_views.PasswordResetCompleteView.as_view(template_name="user/password_reset_completed.html"), name="password_reset_complete"),
 ]
